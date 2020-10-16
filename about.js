10d3795a22d552e0ebd07a64a10cfcf05cc258dc
ayeshani <ayeshanikaushalya@gmail.com>
2020-10-16 19:26:21 +0530

    --- Auto-Git Commit ---

---
+++ about.js
@@ -0,0 +1,28 @@
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+$(function() {
+    
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
---
+++ first_page.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
+
+
+  /*search bar*/
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $(".input").removeClass("active");
+    });
+});
+
+
+
+
